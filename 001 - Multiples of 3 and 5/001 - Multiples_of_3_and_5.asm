66bc787d64d528e86ace391281a4b503f7b716e5
Umut Ozel <umutozel@gmail.com>
2017-01-07 18:53:03 +0300

    Adding 3rd solution
    Adding comments to all solutions

--- 001 - Multiples of 3 and 5/001 - Multiples_of_3_and_5.asm
+++ 001 - Multiples of 3 and 5/001 - Multiples_of_3_and_5.asm
@@ -2,30 +2,31 @@ format PE console
 include "win32ax.inc"
 
   start:
-	mov ecx, 999
+	mov ecx, 999		; load the start number to counter
 	
   .loop:
-	xor edx, edx
-	mov eax, ecx
-	mov ebx, 3
-	div ebx
-	cmp edx, 0
-	jz start.success
+	xor edx, edx		; reset edx. we want 32 bit result, so we are doing 64 bit division
+	mov eax, ecx		; prepare division
+	mov ebx, 3		; load first divisor (3)
+	div ebx 		; divide
+	cmp edx, 0		; check if there is remainder
+	jz start.success	; if not, skip next division
 
-	xor edx, edx
-	mov eax, ecx
-	mov ebx, 5
-	div ebx
-	cmp edx, 0
-	jnz start.skip
+	xor edx, edx		; again, reset edx
+	mov eax, ecx		; prepare division
+	mov ebx, 5		; load second divisor (5)
+	div ebx 		; divide
+	cmp edx, 0		; check if there is remainder
+	jnz start.skip		; if so, skip this number
 	
   .success:
-	add [sum], ecx
+	add [sum], ecx		; add number to variable
 
   .skip:
-	dec ecx
-	jnz start.loop
+	dec ecx 		; decrement counter
+	jnz start.loop		; go to start
 
+	; print sum using printf function of c
 	cinvoke printf, formatstring, [sum]
 	invoke	ExitProcess, 0
 
